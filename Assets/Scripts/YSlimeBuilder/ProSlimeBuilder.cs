c1240952b91c1f838737818d7091cdf60b6b6bfb
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2025-06-22 09:32:41 -0700

    EdgeSorting expensive style

--- Assets/Scripts/YSlimeBuilder/ProSlimeBuilder.cs
+++ Assets/Scripts/YSlimeBuilder/ProSlimeBuilder.cs
@@ -5,10 +5,15 @@ public class ProSlimeBuilder : MonoBehaviour
 {
     public SpriteRenderer rend;
     public List<Vector2> vertices;
+    public List<Vector3> sortedVerts;
 
     public GameObject core;
     public GameObject particle;
     public List<GameObject> points;
+
+    [SerializeField]
+    public SortedDictionary <float, Vector3> vertDict;
+
 
 
     #region builder functions
@@ -23,6 +28,23 @@ public class ProSlimeBuilder : MonoBehaviour
         vertices = new List<Vector2>(rend.sprite.vertices);
     }
 
+    [ContextMenu("Sort Vertices")]
+    void SortVertices()
+    {
+        vertDict = new SortedDictionary<float, Vector3>();
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 vert3 = new Vector3(vertices[i].x, vertices[i].y, gameObject.transform.position.z);
+            float theta = Vector3.Angle(gameObject.transform.position + Vector3.right, vert3) * Mathf.Sign(vertices[i].y);
+            vertDict.Add(theta, vert3);
+        }
+
+        Debug.Log($"dictLength={vertDict.Count}");
+        sortedVerts = new List<Vector3>(vertDict.Values);
+
+    }
+
+
     [ContextMenu("Populate Particles")]
     void PopulateParticles()
     {   
@@ -30,36 +52,31 @@ public class ProSlimeBuilder : MonoBehaviour
         GameObject coreAtom = Instantiate(core, gameObject.transform.position, Quaternion.identity);
         coreAtom.transform.parent = gameObject.transform;
 
-
-        //lets generate an offset
-
         //spawn at each mesh vertex 
-        for (int i = 0; i < vertices.Count; i++)
+        for (int i = 0; i < sortedVerts.Count; i++)
         {
+            //Vector3 vertice3 = new Vector3(vertices[i].x, vertices[i].y, gameObject.transform.position.z);
             
-            Vector3 vertice3 = new Vector3(vertices[i].x, vertices[i].y, gameObject.transform.position.z);
-
+            
             float offset = particle.transform.localScale.x/2;
-
-            Vector3 towardCenter = -vertice3.normalized * offset;
-
-            Debug.Log($"Offset: desired = 0.125 || actual = {offset}");
-            Debug.Log($"towardCenter: vertice3 = {vertice3} || towardCenter = {towardCenter}");
-            Debug.Log($"");
-            GameObject atom = Instantiate(particle, gameObject.transform.position + vertice3 + towardCenter, Quaternion.identity);
+            Vector3 towardCenter = -sortedVerts[i].normalized * offset;
+            GameObject atom = Instantiate(particle, gameObject.transform.position + sortedVerts[i] + towardCenter, Quaternion.identity);
            
             atom.transform.parent = gameObject.transform;
             points.Add(atom);
         }   
-
-         
     }
 
-    [ContextMenu("CleanUp")]
-    void CleanUp()
-    {
 
-    }
+    /* todo
+     * 
+     * clean up outdated builder files before pushing to master
+     * 
+     * fill & lattice structure
+     * 
+     * 
+     */
+
 
     #endregion
 }
