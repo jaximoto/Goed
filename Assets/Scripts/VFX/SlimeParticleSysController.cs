49a7120bd3dbfbecbf43e212229500817954a7c0
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2025-06-28 10:00:30 -0700

    ground trail prefab

--- Assets/Scripts/VFX/SlimeParticleSysController.cs
+++ Assets/Scripts/VFX/SlimeParticleSysController.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.Android;
 
 public class SlimeParticleSysController : MonoBehaviour
 {
     SlimeController _slimeController;
     ParticleSystem _particleSystem;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _slimeController = gameObject.transform.parent.GetComponent<SlimeController>();
         _particleSystem = gameObject.GetComponent<ParticleSystem>();
+        
     }
 
     // Update is called once per frame
@@ -22,11 +25,20 @@ public class SlimeParticleSysController : MonoBehaviour
 
     void SpawnerToGround()
     {
+        var emission = _particleSystem.emission;
         if (_slimeController._grounded)
         {
-            _particleSystem = GetComponent<ParticleSystem>();
-            _particleSystem.transform.position = _slimeController.transform.position + (Vector3.down * gameObject.transform.parent.localScale.x);
-            _particleSystem.transform.up = Vector3.up; 
+            RaycastHit2D hit = Physics2D.Raycast(_slimeController.transform.position, Vector3.down, 1f, ~_slimeController.groundCheckIgnoreLayers.value);
+            if (hit)
+            {
+                emission.enabled = true;
+                _particleSystem.transform.position = hit.point;
+                _particleSystem.transform.up = Vector3.up;
+            }   
+        }
+        else 
+        {
+            emission.enabled = false;
         }
     }
 
