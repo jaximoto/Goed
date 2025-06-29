413735fb70dbb6e5ae4c628075e6f421bc2f249d
jaximoto <jaximods@gmail.com>
2025-06-29 14:43:53 -0700

    got impact sfx

--- Assets/Scripts/YSlimeProtoScripts/SlimeController.cs
+++ Assets/Scripts/YSlimeProtoScripts/SlimeController.cs
@@ -428,7 +428,7 @@ public class SlimeController : MonoBehaviour
             ExecuteJump();
             // interupting
 
-            SoundManager.PlayRandomSoundPitch(SoundType.JUMP, .1f, true);
+            SoundManager.PlayRandomSoundPitch(SoundType.JUMP, .1f, true, .9f, 1.5f);
         }
         
 
